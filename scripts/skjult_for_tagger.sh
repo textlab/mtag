f2f78766d68fe2ad299b03e95ee58ba5fcc75604
Michał Kosek <mihao@users.noreply.github.com>
2019-06-06 21:29:17 +0200

    Specify exceptions to the list of hidden words

--- scripts/skjult_for_tagger.sh
+++ scripts/skjult_for_tagger.sh
@@ -4,15 +4,25 @@ while read id rest; do
     continue
   fi
   hidden[$id]=1
-done < <(grep -v 'tusenvis\|hilse' ../data/skjult_for_tagger_bm.tsv)
+done <../data/skjult_for_tagger_bm.tsv
 # "kris"
 hidden[37785]=1
+
+# Exceptions (i.e. words from skjult_for_tagger_bm that shouldn't actually be hidden):
+while read id rest; do
+  hidden[$id]=0
+done <../data/inn-igjen-i-ordbanken-bm.txt
 
 while read id rest; do
   if [ "$id" = "Lemma-id" ]; then
     continue
   fi
   hidden_nn[$id]=1
-done < <(cat ../data/skjult_for_tagger_nn.tsv)
+done <../data/skjult_for_tagger_nn.tsv
 # "kris"
 hidden_nn[51007]=1
+
+# Exceptions (i.e. words from skjult_for_tagger_nn that shouldn't actually be hidden):
+while read id rest; do
+  hidden_nn[$id]=0
+done <../data/inn-igjen-i-ordbanken-nn.txt
